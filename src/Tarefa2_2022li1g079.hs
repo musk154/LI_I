d5c7af25ce8d8ff253310a59109c96538b31a43f
a104530 <a104530@alunos.uminho.pt>
2022-11-08 15:19:58 +0000

    Tarefa 2 ainda sem a aleatoridade

--- src/Tarefa2_2022li1g079.hs
+++ src/Tarefa2_2022li1g079.hs
@@ -11,10 +11,42 @@ module Tarefa2_2022li1g079 where
 import LI12223
 
 estendeMapa :: Mapa -> Int -> Mapa
-estendeMapa = undefined
+estendeMapa m n = 
 
 proximosTerrenosValidos :: Mapa -> [Terreno]
-proximosTerrenosValidos = undefined
+proximosTerrenosValidos (Mapa l []) = [Estrada 0, Rio 0, Relva]
+proximosTerrenosValidos (Mapa l ((Rio y,x):(Rio y1,x1):(Rio y2,x2):(Rio y3,x3):t)) = [Estrada 0,Relva]
+proximosTerrenosValidos (Mapa l ((Rio y,x):t)) = [Estrada 0, Rio 0, Relva]
+proximosTerrenosValidos (Mapa l ((Estrada y,x):(Estrada y1,x1):(Estrada y2,x2):(Estrada y3,x3):(Estrada y4,x4))) = [Rio 0,Relva]
+proximosTerrenosValidos (Mapa l ((Relva x):(Relva x1):(Relva x2):(Relva x3):(Relva x4))) = [Estrada 0, Rio 0]
+proximosTerrenosValidos (Mapa l ((Estrada y,x):t)) = [Estrada 0, Rio 0, Relva ]
+proximosTerrenosValidos (Mapa l ((Relva x)):t) = [Estrada 0, Rio 0, Relva]
+
+
+
 
 proximosObstaculosValidos :: Int -> (Terreno, [Obstaculo]) -> [Obstaculo]
-proximosObstaculosValidos = undefined
+proximosObstaculosValidos n (x,y) | length x == n = []
+                                  | length x == (n-1) && (elem Nenhum x) == False = [Nenhum]
+                                  | otherwise = proximosObstaculosValidos n (x,y)
+
+proximosObstaculosValidosaux :: Int -> (Terreno, [Obstaculo]) -> [Obstaculo]
+proximosObstaculosValidosaux n ((Rio y),x) = [Nenhum, Tronco]
+proximosObstaculosValidosaux n ((Relva),x) = [Nenhum, Arvore]
+proximosObstaculosValidosaux n ((Rio y),(Tronco:Tronco:Tronco:Tronco:Tronco:t)) = [Nenhum]
+proximosObstaculosValidosaux n ((Estrada y),(Carro:Carro:Carro:t)) = [Nenhum]
+proximosObstaculosValidosaux n ((Estrada y),x) = [Nenhum, Carro]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
