52f3c5003dfb7cf57ca9ce41689346def647a072
a104530 <a104530@alunos.uminho.pt>
2022-11-12 13:47:33 +0000

    Adicionei a função que movimenta o jogador e a função de mover os troncos consoante a velocidade do rio

--- src/Tarefa3_2022li1g079.hs
+++ src/Tarefa3_2022li1g079.hs
@@ -13,7 +13,30 @@ import LI12223
 animaJogo :: Jogo -> Jogada -> Jogo
 animaJogo = undefined
 
+{- | a função player determina as próximas coordenadas do jogador, consoante a sua posição no mapa e a existência de obstaculos,
+na primeira linha é verificado que se o jogador se encontrar em cima de um tronco e não efetuar nenhuma jogada (Parado), 
+movimenta-se consoante a velocidade do tronco.
 
+Obs: na ultima linha o "otherwise" representa o movimento "Esquerda"
+-}
+
+
+player :: Jogador -> Mapa -> Jogada -> Jogador
+player (Jogador (x,y)) (Mapa l tl) (Parado) | elem (x,y) (posT (Mapa l tl) (0,0)) = (Jogador (posMoveT (x,y) (Mapa l tl)))
+                                            | otherwise = (Jogador (x,y))
+
+player (Jogador (x,y)) (Mapa l tl) (Move jog) | jog == Cima && elem ((x-1),y) (posA (Mapa l tl) (0,0)) = (Jogador (x,y))
+                                              | jog == Baixo && elem ((x+1),y) (posA (Mapa l tl) (0,0)) = (Jogador (x,y))
+                                              | jog == Direita && elem (x,(y+1)) (posA (Mapa l tl) (0,0)) = (Jogador (x,y))
+                                              | jog == Esquerda && elem (x,(y-1)) (posA (Mapa l tl) (0,0)) = (Jogador (x,y))
+                                              | jog == Cima && x == 0 = (Jogador (x,y))
+                                              | jog == Baixo && x == ((length tl) - 1) = (Jogador (x,y))
+                                              | jog == Direita && y == (l-1) = (Jogador (x,y))
+                                              | jog == Esquerda && y == 0 = (Jogador (x,y))
+                                              | jog == Cima = (Jogador ((x-1),y))
+                                              | jog == Baixo = (Jogador ((x+1),y))
+                                              | jog == Direita = (Jogador (x,(y+1)))
+                                              | otherwise = (Jogador (x,(y-1)))
 
 
 
@@ -55,6 +78,41 @@ posAaux :: (Terreno,[Obstaculo]) -> Coordenadas -> [Coordenadas]
 posAaux (Relva,[]) (x,y) = []
 posAaux (Relva,(h:t)) (x,y) | h == Arvore = ((x,y):posAaux (Relva,t) (x,(y+1)))
                             | otherwise = posAaux (Relva,t) (x,(y+1))
+
+{- | a função posMoveT é a função responsável por movimentar os troncos do mapa consoante a velocidade do terreno -}
+
+
+posMoveT :: Coordenadas -> Mapa -> Coordenadas
+posMoveT (x,y) (Mapa l []) = (x,y)
+posMoveT (x,y) (Mapa l ((Relva,ob):t)) = posMoveT (x,y) (Mapa l t) 
+posMoveT (x,y) (Mapa l ((Estrada vel,ob):t)) = posMoveT (x,y) (Mapa l t)
+posMoveT (x,y) (Mapa l ((Rio vel, ob):t)) | elem (x,y) (posT (Mapa l ((Rio vel, ob):t)) (0,0)) = posMoveVelocidade (x,y) (Mapa l ((Rio vel, ob):t))
+                                          | otherwise = posMoveT (x,y) (Mapa l t)
+
+
+{- | a função posMoveVelocidade é uma função auxiliar da função posMoveT que verifica se a proxima posição de um tronco
+respeita as fronteiras do mapa -}
+
+
+posMoveVelocidade :: Coordenadas -> Mapa -> Coordenadas
+posMoveVelocidade (x,y) (Mapa l ((Rio vel,ob):t)) | y + vel < 0 = (x,(y+(l-vel)))
+                                                  | y + vel >= l = (x,((y-l)+vel))
+                                                  | otherwise = (x,y+vel)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
