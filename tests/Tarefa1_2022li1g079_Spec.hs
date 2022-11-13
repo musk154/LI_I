a0b9e85a3797d0c98f1d97c106c1c9f85112f79f
a103997 <a103997@alunos.uminho.pt>
2022-11-13 22:09:01 +0000

    Testes da tarefa 1 e 4

--- tests/Tarefa1_2022li1g079_Spec.hs
+++ tests/Tarefa1_2022li1g079_Spec.hs
@@ -4,5 +4,14 @@ import LI12223
 import Tarefa1_2022li1g079
 import Test.HUnit
 
+
 testsT1 :: Test
-testsT1 = TestLabel "Testes Tarefa 1" $ test ["Teste 1" ~: 1 ~=? 1]
+testsT1 = TestLabel "Testes Tarefa 1" $ test ["Teste 1- Três terrenos diferentes com os obstáculos corretos, caso normal" ~: True ~=? mapaValido (Mapa 4 [(Relva, [Arvore, Nenhum, Arvore, Nenhum]), (Estrada 2 , [Carro, Nenhum, Nenhum, Nenhum]), (Rio 2, [Tronco, Tronco, Nenhum, Nenhum])])
+                                             ,"Teste 2- Tem um carro no terreno rio por isso é inválido" ~: False ~=? mapaValido (Mapa 4 [(Relva, [Arvore, Nenhum, Arvore, Nenhum]), (Estrada 2 , [Carro, Nenhum, Nenhum, Nenhum]), (Rio 2, [Tronco, Tronco, Nenhum, Carro])])
+                                             ,"Teste 3- Tem um tronco no terreno Estrada assim, é inválido" ~: False ~=? mapaValido (Mapa 4 [(Relva, [Arvore, Nenhum, Arvore, Nenhum]), (Estrada 2 , [Carro, Nenhum, Nenhum, Tronco]), (Rio 2, [Tronco, Tronco, Nenhum, Nenhum])])
+                                             ,"Teste 4- A largura do mapa nao coincide com o numero de obstaculos" ~: False ~=? mapaValido (Mapa 2 [(Relva, [Arvore, Nenhum, Arvore, Tronco]), (Estrada 2 , [Carro, Nenhum, Nenhum, Nenhum]), (Rio 2, [Tronco, Tronco, Nenhum, Carro])])
+                                             ,"Teste 5- Existem dois rios contiguos com a mesma direçao" ~: False ~=? mapaValido (Mapa 4 [(Rio 2, [Tronco, Nenhum, Tronco, Carro]), (Rio 2, [Tronco, Tronco, Nenhum, Carro]), (Estrada 2 , [Carro, Nenhum, Nenhum, Nenhum])])
+                                             ,"Teste 6- Existem  carros a ocupar 4 unidades seguidas" ~: False ~=? mapaValido (Mapa 5 [(Relva, [Arvore, Nenhum, Arvore, Nenhum, Nenhum]), (Estrada 2 , [Carro, Carro, Carro, Carro, Nenhum]), (Rio 2, [Tronco, Tronco, Nenhum, Nenhum, Tronco])])
+                                             ,"Teste 7- Existem troncos com mais de 6 unidades seguidas" ~: False ~=? mapaValido (Mapa 7 [(Relva, [Arvore, Nenhum, Arvore, Nenhum, Nenhum, Arvore, Nenhum]), (Estrada 2 , [Carro, Nenhum, Nenhum, Carro, Nenhum, Nenhum, Carro]), (Rio 2, [Tronco, Tronco, Tronco, Tronco, Tronco, Tronco, Nenhum])])
+                                             ,"Teste 8- Numa das linhas do mapa, nao existe nenhum obstaculo do tipo Nenhum" ~: False ~=? mapaValido (Mapa 4 [(Rio 2, [Tronco, Tronco, Tronco, Tronco]), (Relva, [Arvore, Arvore, Nenhum, Nenhum]), (Estrada 2 , [Carro, Nenhum, Nenhum, Nenhum])])
+                                             ,"Teste 9- Existem mais de cinco relvas seguidas" ~: False ~=? mapaValido (Mapa 4 [(Relva, [Arvore, Nenhum, Arvore, Nenhum]), (Relva, [Arvore, Nenhum, Arvore, Nenhum]), (Relva, [Arvore, Nenhum, Arvore, Nenhum]), (Relva, [Arvore, Nenhum, Arvore, Nenhum]), (Relva, [Arvore, Nenhum, Arvore, Nenhum]), (Relva, [Arvore, Nenhum, Arvore, Nenhum]), (Estrada 2 , [Carro, Nenhum, Nenhum, Nenhum]), (Rio 2, [Tronco, Tronco, Nenhum, Carro])])]
--- tests/Tarefa4_2022li1g079_Spec.hs
+++ tests/Tarefa4_2022li1g079_Spec.hs
@@ -5,4 +5,8 @@ import Tarefa4_2022li1g079
 import Test.HUnit
 
 testsT4 :: Test
-testsT4 = TestLabel "Testes Tarefa 4" $ test ["Teste 1" ~: 1 ~=? 1]
+testsT4 = TestLabel "Testes Tarefa 4" $ test ["Teste 1- É atropelado por isso devolve True" ~: True ~=? jogoTerminou (Jogo (Jogador (1,2)) (Mapa 4 [(Rio 2, [Nenhum, Tronco, Nenhum, Tronco]), (Estrada 2, [Carro, Carro, Carro, Nenhum]), (Relva, [Arvore, Nenhum, Arvore, Nenhum])]))
+                                             ,"Teste 2- O jogador esta fora dos limites do mapa no x, por isso o jogo termina e da True" ~: True ~=? jogoTerminou (Jogo (Jogador (4,2)) (Mapa 4 [(Rio 2, [Nenhum, Tronco, Nenhum, Tronco]), (Estrada 2, [Carro, Carro, Carro, Nenhum]), (Relva, [Arvore, Nenhum, Arvore, Nenhum])]))
+                                             ,"Teste 3- O jogador esta fora dos limites do mapa no y, por isso o jogo termina e da True" ~: True ~=? jogoTerminou (Jogo (Jogador (0,7)) (Mapa 4 [(Rio 2, [Nenhum, Tronco, Nenhum, Tronco]), (Estrada 2, [Carro, Carro, Carro, Nenhum]), (Relva, [Arvore, Nenhum, Arvore, Nenhum])]))
+                                             ,"Teste 4- O jogador caiu na agua, por isso o jogo termina e da True" ~: True ~=? jogoTerminou (Jogo (Jogador (0,0)) (Mapa 4 [(Rio 2, [Nenhum, Tronco, Nenhum, Tronco]), (Estrada 2, [Carro, Carro, Carro, Nenhum]), (Relva, [Arvore, Nenhum, Arvore, Nenhum])]))
+                                             ,"Teste 5- O jogador econtra-se num Tronco por isso nao morre, e da False" ~: False ~=? jogoTerminou (Jogo (Jogador (0,1)) (Mapa 4 [(Rio 2, [Nenhum, Tronco, Nenhum, Tronco]), (Estrada 2, [Carro, Carro, Carro, Nenhum]), (Relva, [Arvore, Nenhum, Arvore, Nenhum])]))]
