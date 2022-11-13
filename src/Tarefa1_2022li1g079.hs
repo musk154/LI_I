d2edfd0473cab01a9a18bb88b66e1fd08976f785
a103997 <a103997@alunos.uminho.pt>
2022-11-13 22:31:59 +0000

    Correçao

--- src/Tarefa1_2022li1g079.hs
+++ src/Tarefa1_2022li1g079.hs
@@ -11,7 +11,7 @@ module Tarefa1_2022li1g079 where
 
 import LI12223
 
-{-/
+{-|
 A função mapaValido é a função principal e funciona a partir da junção das várias funções auxiliares. (Se o resultado de todas as funções auxiliares for Verdadeiro, então a função mapaValido também o será.)
 @mapaValido :: Mapa -> Bool
 mapaValido (Mapa c ((tr, (x:y)):t)) = mapaValido1 (Mapa c ((tr, (x:y)):t)) && mapaValido2 (Mapa c ((tr, (x:y)):t)) && mapaValido3 (Mapa c ((tr, (x:y)):t)) && mapaValido4 (Mapa c ((tr, (x:y)):t)) && mapaValido5 (Mapa c ((tr, (x:y)):t)) && mapaValido6 (Mapa c ((tr, (x:y)):t)) && mapaValido7 (Mapa c ((tr, (x:y)):t))@-}
@@ -19,7 +19,7 @@ mapaValido (Mapa c ((tr, (x:y)):t)) = mapaValido1 (Mapa c ((tr, (x:y)):t)) && ma
 mapaValido :: Mapa -> Bool
 mapaValido (Mapa c ((tr, (x:y)):t)) = mapaValido1 (Mapa c ((tr, (x:y)):t)) && mapaValido2 (Mapa c ((tr, (x:y)):t)) && mapaValido3 (Mapa c ((tr, (x:y)):t)) && mapaValido4 (Mapa c ((tr, (x:y)):t)) && mapaValido5 (Mapa c ((tr, (x:y)):t)) && mapaValido6 (Mapa c ((tr, (x:y)):t)) && mapaValido7 (Mapa c ((tr, (x:y)):t))
 
-{- A função mapaValido1 certifica que não existem obstáculos em terrenos impróprios. 
+{-| A função mapaValido1 certifica que não existem obstáculos em terrenos impróprios. 
 Na primeira linha tem o caso de paragem, onde se a lista de [Terreno, (Obstáculos)] está vazia e por isso não há erros.
 Nas linhas seguintes é determinado o que fazer no caso dos três terrenos possíveis. Na estrada, os obstáculos possíveis são carros, na relva, as árvores e nos rios, troncos.
 Caso algum dos elementos que não pertence ao respetivo terreno se encontrar na lista de obstáculos (utilizando a função elem), o mapa deixa de ser válido. Por exemplo, não podem existir nem árvores, nem carros no rio.
@@ -49,7 +49,7 @@ mapaValido1 (Mapa c (( Relva, (x:y)):t))
     |(elem Tronco (x:y) || elem Carro (x:y)) = False
     | otherwise = mapaValido1 (Mapa c t)
 
-{-/
+{-|
 A função mapaValido2 verifica se existem rios contíguos com a mesma direção (o que não pode acontecer).
 Os rios, assim como a estrada, tem velocidade associada ao terreno pois estes tem movimento. Se a velocidade for um valor positivo então os obstáculos movem-se da esquerda para a direita, se for negativo é o contrário.
 Para verificar se os rios contíguos tem a mesma direção, avaliamos a velocidade (se são valores ambos positivos, ambos negativos ou os dois diferentes).
@@ -84,7 +84,7 @@ mapaValido2 (Mapa c ((Rio n, (x:y)): (Rio n', (x':y')): t))
     | n < 0 && n' < 0 = False
     |otherwise = mapaValido2 (Mapa c ((Rio n', (x':y')):t))
 
-{-/
+{-|
 Na função mapaValido3 verifamos que os troncos não tem mais de 5 unidades.
 Como nas funções vistas até agora, o caso de paragem na primeira linha acontece por não ter erro nenhum, a lista de obstáculos está vazia.
 No caso de o primeiro terreno ser relva ou estrada, que não estamos a avaliar nesta função, a função irá avaliar o resto da lista (Terreno,[Obstáculo]) (recursividade).
@@ -139,7 +139,7 @@ contaAux [] = 0
 contaAux ((x:y):t) | (elem Tronco (x:y)) && (length (x:y) >= (contaAux t)) = length (x:y)
                    | otherwise = contaAux t
 
-{-/
+{-|
 Pela mesma lógica que a função mapaValido3, para função mapaValido4, que pretende verificar se não existem carros com mais de três unidades, utilizamos a função auxilar conta (criada em cima) e
 a função contaAux1 que é uma variação da função contaAux mas procura e devolve o numero de elementos com maior numero de Carros, e não Troncos, das listas resultantes da primeira função.
 Na primeira linha está o caso de paragem semelhante ás funções vistas até ao momento (se a lista não contém nada, não pode dar erro).
@@ -184,7 +184,7 @@ contaAux1 [] = 0
 contaAux1 ((x:y):t) | (elem Carro (x:y)) && (length (x:y) >= (contaAux1 t)) = length (x:y)
                     | otherwise = contaAux1 t                                        
                     
-{-/
+{-|
 mapaValido5 verifica se existe alguma linha do mapa constituída exclusivamente por obstáculos sem espaços vazios (Nenhum).
 Caso de paragem na primeira linha, caso a lista se vazia o mapa é válido.
 Quando recebe um mapa, procura se existe algum elemento na lista dos obstáculos (independetemente do terreno) que seja Nenhum. Se não existir, o mapa não é válido (False == False).
@@ -199,7 +199,7 @@ mapaValido5 (Mapa c []) = True
 mapaValido5 (Mapa c ((tr, (x:y)):t))| elem Nenhum (x:y) == False = False
                                     | otherwise = mapaValido5 (Mapa c t)
 
-{-/
+{-|
 A função mapaValido6 compara a largura do mapa com o número de elementos dos obstáculos de cada terreno/linha (comprimento da lista). 
 O caso de paragem irá dar mapa válido pois a lista está vazia.
 Quando recebe um Mapa (onde a lista não está vazia), a função vai comparar a largura atribuida ao Mapa (c) com o comprimento da lista dos obstáculos. Se forem diferentes, o mapa vai dar inválido,
@@ -217,7 +217,7 @@ mapaValido6 (Mapa c ((tr, (x:y)):t)) | c /= length (x:y) = False
                                      | otherwise = mapaValido6 (Mapa c t)
 
 
-{-/
+{-|
 A função mapaValido7 verifica se exitem mais de 4 rios seguidos e/ou mais de 5 Estradas ou Relva.
 O caso de paragem é o mesmo das outras funções, uma vez que a lista vazia cumpre a condição.
 Para os vários terrenos utilizamos uma funçao auxiliar que agrupa em listas os elementos iguais de terrenos e obstáculos iguais do mapa dado (conta 7).
--- src/Tarefa4_2022li1g079.hs
+++ src/Tarefa4_2022li1g079.hs
@@ -10,7 +10,7 @@ module Tarefa4_2022li1g079 where
 
 import LI12223
 
-{-/ A funçao jogoTerminou verifica se o jogador terminou o jogo ou nao de acordo com: as coordenadas nao coicidirem com os limites do mapa, estar em nenhum no terreno rio e ter as mesmas coordenadas que um carro no terreno estrada.
+{-| A funçao jogoTerminou verifica se o jogador terminou o jogo ou nao de acordo com: as coordenadas nao coicidirem com os limites do mapa, estar em nenhum no terreno rio e ter as mesmas coordenadas que um carro no terreno estrada.
 Assim utiliza tres funçoes auxiliares definidas a baixo para determinar a sua validade.
 @jogoTerminou :: Jogo -> Bool
 jogoTerminou (Jogo (Jogador (x,y)) (Mapa c ((tr, (x':y')):t))) = jogoTerminoumapa (x,y) (Mapa c ((tr, (x':y')):t)) || jogoTerminouAgua (x,y) (Mapa c ((tr, (x':y')):t)) || jogoTerminouCarro (x,y) (Mapa c ((tr, (x':y')):t))
@@ -19,7 +19,7 @@ jogoTerminou (Jogo (Jogador (x,y)) (Mapa c ((tr, (x':y')):t))) = jogoTerminoumap
 jogoTerminou :: Jogo -> Bool
 jogoTerminou (Jogo (Jogador (x,y)) (Mapa c ((tr, (x':y')):t))) = jogoTerminoumapa (x,y) (Mapa c ((tr, (x':y')):t)) || jogoTerminouAgua (x,y) (Mapa c ((tr, (x':y')):t)) || jogoTerminouCarro (x,y) (Mapa c ((tr, (x':y')):t))
 
-{-/
+{-|
 A funçao jogoTerminoumapa verifica se o jogador/ personagem se encontra fora dos limites do mapa. Assim, recebe as coordenadas do jogador, o mapa para comparaçao e devolve um Bool. Caso seja True, o jogador perdeu o jogo.
 Comparamos: o x (posiçao do jogador na linha- verticalmente) com a largura do mapa, ou seja, quantos obstaculos estao definidos; o y (posiçao do jogador na coluna- horizontalmente) com o comprimento da lista de [Terreno, (Obstaculos)] (quantos terrenos existem). 
 Como a primeira posiçao é 0 e nao 1, usamos o >= (maior ou igual) pois no caso de serem iguais o jogador estara fora do mapa. 
@@ -33,7 +33,7 @@ jogoTerminoumapa :: Coordenadas -> Mapa -> Bool
 jogoTerminoumapa (x,y) (Mapa c ((tr, (x':y')):t)) | x >= c || y >= length ((tr, (x':y')):t) = True
                                                   | otherwise = False
 
-{-/
+{-|
 A funçao jogoTerminouAgua determina se o jogo termina pois o jogador caiu na agua ou nao.
 Se o primeiro terreno for rio, utilizando uma funçao auxiliar (ondeTronco) que indica as coordenadas todas de onde nao ha tronco no mapa, ou seja, onde tem Nenhum, a funçao irá procurar se nas coordenadas dadas (do jogador) existe nessas coordenadas um tronco.
 Caso exista um tronco nessas coordenadas, o jogador nao morre, ou seja false. De seguida verifica o resto da funçao mas com as coordenadas (x-1,y) pois a linha analid¡sada com o movimento do mapa desaparece e passamos para a seguinte.
@@ -59,7 +59,7 @@ ondenaoTronco :: (Terreno, [Obstaculo]) -> Coordenadas -> [Coordenadas]
 ondenaoTronco (Rio n, []) (x,y) = []
 ondenaoTronco (Rio n, (x':y')) (x,y) = if x' == Nenhum then (x,y) : ondenaoTronco (Rio n, y') (x, y+1) else ondenaoTronco (Rio n, y') (x, y+1)
 
-{-/
+{-|
 A semelhança da funçao anterior, a funçao jogoTerminouCarro verifica se o jogador se encontra nas mesmas coordenadas que um carro, ou seja é atropelado ou nao.
 Tambem utiliza uma funçao auxiliar que dá as coordenadas dos carros (ondeCarro).
 Neste caso, compara as coordenadas do jogador com as coordenadas obtidas na funçao auxiliar mas no caso de coicidirem, o jogo termina pois o jogador é atropelado.
