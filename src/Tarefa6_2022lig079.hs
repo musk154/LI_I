7184e319460826b0937dbe6385a0dcd998b809f0
a104530 <a104530@alunos.uminho.pt>
2022-12-21 11:35:00 +0000

    Algumas alterações, principalmente estéticas

--- src/Tarefa6_2022lig079.hs
+++ src/Tarefa6_2022lig079.hs
@@ -3,7 +3,6 @@ module Main where
 
 import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game
-import Graphics.Gloss.Interface.IO.Game
 
 
 import LI12223
@@ -28,7 +27,7 @@ type Pont = Float
 type Imagem = [Picture]
 
 window :: Display
-window = InWindow "Teste" (1000, 1000) (0,0)
+window = FullScreen --InWindow "Teste" (1000, 1000) (0,0)
 
 fr :: Int
 fr = 50
@@ -53,13 +52,13 @@ mapaInicial = (Mapa 10 [(Rio 2, [n,n,t,t,t,n,n,t,n,t]),
 
 	                     
 estadoInicial :: Imagem -> World
-estadoInicial imagem = (Opcoes Jogar, Jogo (Jogador (0,-437)) (mapaInicial), imagem, 0)
+estadoInicial imagem = (Opcoes Jogar, Jogo (Jogador (-50,-425)) (mapaInicial), imagem, 0)
 
 
 desenhaEstado :: World -> Picture
 desenhaEstado (PerdeuJogo, jogo, imagem, pont) = Translate (-50) 0 $ Color red $ scale 0.5 0.5 $ Text ("Score: " ++ show (round pont))
-desenhaEstado (Opcoes Jogar, jogo, imagem, pont) = Pictures [Color red $ desenhaOp "Jogar", Translate 0 (-200) $  desenhaOp "Fechar"]
-desenhaEstado (Opcoes Sair, jogo, imagem, pont) = Pictures [desenhaOp "Jogar", Color red $ Translate 0 (-200) $ desenhaOp "Fechar"]
+desenhaEstado (Opcoes Jogar, jogo, imagem, pont) = Pictures [Color red $ desenhaOp "Jogar", Translate (-45) (-200) $  desenhaOp "Fechar"]
+desenhaEstado (Opcoes Sair, jogo, imagem, pont) = Pictures [desenhaOp "Jogar", Color red $ Translate (-45) (-200) $ desenhaOp "Fechar"]
 desenhaEstado (ModoJogo, Jogo (Jogador (x,y)) (mapaInicial),imagem,n) = Pictures $ (desenhaMapa mapaInicial (-900) (-425)) ++ [Translate i j $ player]
    where 
    	 i = fromIntegral x
@@ -69,26 +68,26 @@ desenhaEstado (ModoJogo, Jogo (Jogador (x,y)) (mapaInicial),imagem,n) = Pictures
 
 desenhaMapa :: Mapa -> Float -> Float -> [Picture]
 desenhaMapa (Mapa l []) x y = [circle 1]
-desenhaMapa (Mapa l ((ter,ob):t)) x y | ter == Relva = (Translate x y $ (color green $ rectangleSolid 9999 100)) : desenhaMapa (Mapa l t) x (y + 100)
+desenhaMapa (Mapa l ((ter,ob):t)) x y | ter == Relva = (Translate x y $ (color corRelva $ rectangleSolid 9999 90)) : desenhaMapa (Mapa l t) x (y + 90)
                                       | otherwise = desenhaMapaAux (Mapa l ((ter, ob):t)) x y
 
 desenhaMapaAux :: Mapa -> Float -> Float -> [Picture]
-desenhaMapaAux (Mapa l ((Estrada vel, ob):t)) x y = (Translate x y $ (color (greyN 0.4) $ rectangleSolid 9999 100)) : desenhaMapa (Mapa l t) x (y+100)
-desenhaMapaAux (Mapa l ((Rio vel, ob):t)) x y = (Translate x y $ (color blue $ rectangleSolid 9999 100)) : desenhaMapa (Mapa l t) x (y+100)
+desenhaMapaAux (Mapa l ((Estrada vel, ob):t)) x y = (Translate x y $ ( color (corEstrada) $ rectangleSolid 9999 90)) : desenhaMapa (Mapa l t) x (y+90)
+desenhaMapaAux (Mapa l ((Rio vel, ob):t)) x y = (Translate x y $ ( color corRio $ rectangleSolid 9999 90)) : desenhaMapa (Mapa l t) x (y+90)
 
 
 
-desenhaOp opc = Translate 0 50 $ Text opc
+desenhaOp opc = Translate (-100) 50 $ Text opc
 
 
 novoEstado :: Key -> (Int,Int) -> Jogo
 novoEstado key (x,y) =
 	let p = (x + dx, y + dy)
 	    (dx,dy) = case key of
-	     	(SpecialKey KeyUp) -> (0,100)
-	     	(SpecialKey KeyDown) -> (0,-100)
-	     	(SpecialKey KeyLeft) -> (-100,0)
-	     	(SpecialKey KeyRight) -> (100,0)
+	     	(SpecialKey KeyUp) -> (0,90)
+	     	(SpecialKey KeyDown) -> (0,-90)
+	     	(SpecialKey KeyLeft) -> (-90,0)
+	     	(SpecialKey KeyRight) -> (90,0)
 	in Jogo (Jogador p) (mapaInicial)
 
 
@@ -102,10 +101,10 @@ event (EventKey (SpecialKey KeyEnter) Down _ _) (Opcoes Sair, jogo, i, pont) = e
 --contiunar a jogar depois de vencer
 event (EventKey (SpecialKey KeyEnter) Down _ _) (PerdeuJogo, jogo, i ,pont) = estadoInicial i
 --identificar que acabou o jogo
-event _ (ModoJogo, (Jogo(Jogador (1000,y)) (mapaInicial)),i,pont) = (PerdeuJogo,(Jogo(Jogador (1000,y)) (mapaInicial)), i, pont)
-event _ (ModoJogo, (Jogo(Jogador (-1000,y)) (mapaInicial)),i,pont) = (PerdeuJogo,(Jogo(Jogador (-1000,y)) (mapaInicial)), i, pont)
-event _ (ModoJogo, (Jogo(Jogador (x, 525)) (mapaInicial)),i,pont) = (PerdeuJogo,(Jogo(Jogador (x,525)) (mapaInicial)), i, pont)
-event _ (ModoJogo, (Jogo(Jogador (x,-725)) (mapaInicial)),i,pont) = (PerdeuJogo,(Jogo(Jogador (x,-725)) (mapaInicial)), i, pont)
+event _ (ModoJogo, (Jogo(Jogador(x,y)) (mapaInicial)),i,pont) | x >= 700 = (PerdeuJogo,(Jogo(Jogador (700,y)) (mapaInicial)), i, pont)
+                                                              | x <= -700 = (PerdeuJogo,(Jogo(Jogador (700,y)) (mapaInicial)), i, pont)
+                                                              | y <= -600 = (PerdeuJogo,(Jogo(Jogador (x,-600)) (mapaInicial)), i, pont)
+ 
 --Modo Jogo
 event (EventKey key Down _ _) (ModoJogo, (Jogo (Jogador (x,y)) (mapaInicial)), i, pont) = (ModoJogo, novoEstado key (x,y), i, pont)
 --Andar para tras com o tempo
@@ -113,15 +112,32 @@ event _ (ModoJogo, (Jogo (Jogador (x,y)) (mapaInicial)), i, pont) = (ModoJogo, (
 --Nao reagir caso não aconteçam os casos em cima
 event _ x = x
 
+
 pontu :: Float -> World -> World
 pontu p (PerdeuJogo, j, i, pont) = (PerdeuJogo, j, i, pont)
 pontu p (o,j,i,pont) = (o,j,i,pont+p)
 
+
+-------CORES---------
+corFundo :: Color
+corFundo = makeColor 0 188 227 5
+
+corRelva :: Color
+corRelva = makeColor 0 150 0 0.6
+
+corEstrada :: Color
+corEstrada = dim (greyN 0.3)
+
+corRio :: Color
+corRio =  dark $ corFundo
+----------------------
+
+
 main :: IO ()
 main = do
 	pacman <- loadBMP "pac_open.bmp"
-	let imagem = [scale 4 4 pacman]
-	play window (greyN 0.6) fr (estadoInicial imagem) desenhaEstado event pontu
+	let imagem = [scale 5 5 pacman]
+	play window corFundo fr (estadoInicial imagem) desenhaEstado event pontu
 
 
 
