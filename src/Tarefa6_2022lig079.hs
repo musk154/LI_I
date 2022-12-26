d769ad8bc1300a7012adf08df77a2e69c4aec1e2
a103997 <a103997@alunos.uminho.pt>
2022-12-26 02:19:59 +0000

    Novas Personagens

--- src/Tarefa6_2022lig079.hs
+++ src/Tarefa6_2022lig079.hs
@@ -22,11 +22,17 @@ import Tarefa5_2022li1g079
 
 data Opcao = Jogar
             | Sair
+            | Skins
 
 data Menu = Opcoes Opcao
           | ModoJogo 
-          | VenceuJogo
+          | ModoJogo1
           | PerdeuJogo
+          | ModoSkins Botao 
+
+data Botao = Skin1
+            | Skin2
+            | Skin3
 
 type World = (Menu, Jogo, Imagem, Pont)
 
@@ -65,14 +71,22 @@ estadoInicial imagem = (Opcoes Jogar, Jogo (Jogador (0,-475)) (mapaInicial), ima
 
 desenhaEstado :: World -> Picture
 desenhaEstado (PerdeuJogo, jogo, imagem, pont) = Pictures $ [(Translate 0 0 $ (!!) imagem 11)] ++ [Translate (-10) (-200) $ (Text (show (round pont)))]
-desenhaEstado (Opcoes Jogar, jogo, imagem, pont) = Pictures $ [(Translate 0 0 $ (!!) imagem 8)] ++ [(Translate 0 10 $ (!!) imagem 12)] ++ [(Translate 0 0 $ (!!) imagem 9)] ++ [(Translate 0 (-200) $ (!!) imagem 10)]
-desenhaEstado (Opcoes Sair, jogo, imagem, pont) = Pictures $ [(Translate 0 0 $ (!!) imagem 8)] ++ [(Translate 0 (-190) $ (!!) imagem 12)] ++ [(Translate 0 0 $ (!!) imagem 9)] ++ [(Translate 0 (-200) $ (!!) imagem 10)] 
+desenhaEstado (Opcoes Jogar, jogo, imagem, pont) = Pictures $ [(Translate 0 0 $ (!!) imagem 8)] ++ [(Translate 0 10 $ (!!) imagem 12)] ++ [(Translate 0 0 $ (!!) imagem 9)] ++ [(Translate 0 (-200) $ (!!) imagem 13)] ++ [(Translate 0 (-400) $ (!!) imagem 10)]
+desenhaEstado (Opcoes Sair, jogo, imagem, pont) = Pictures $ [(Translate 0 0 $ (!!) imagem 8)] ++ [(Translate 0 (-400) $ (!!) imagem 12)] ++ [(Translate 0 0 $ (!!) imagem 9)] ++ [(Translate 0 (-200) $ (!!) imagem 13)] ++ [(Translate 0 (-400) $ (!!) imagem 10)]
+desenhaEstado (Opcoes Skins, jogo,imagem, pont) = Pictures $ [(Translate 0 0 $ (!!) imagem 8)] ++ [(Translate 0 (-190) $ (!!) imagem 12)] ++ [(Translate 0 0 $ (!!) imagem 9)] ++ [(Translate 0 (-200) $ (!!) imagem 13)] ++ [(Translate 0 (-400) $ (!!) imagem 10)]
+desenhaEstado (ModoSkins Skin1, jogo, imagem, pont) = Pictures $ [(Translate 0 0 $ (!!) imagem 8)] ++ [(Translate 0 (-190) $ (!!) imagem 14)]
+desenhaEstado (ModoSkins Skin2, jogo, imagem, pont) = Pictures $ [(Translate 0 0 $ (!!) imagem 8)] ++ [(Translate 0 (-190) $ (!!) imagem 15)] ++ [(Translate 0 0 $ (!!) imagem 16)]
 desenhaEstado (ModoJogo, Jogo (Jogador (x,y)) (mapaInicial),imagem,n) = Pictures $ (desenhaMapa (ModoJogo, Jogo (Jogador (x,y)) (mapaInicial),imagem,n) (-900) (515)) ++ (desenhaObsAux (ModoJogo, Jogo (Jogador (x,y)) (mapaInicial),imagem,n) (-900) (515)) ++ [Translate i j $ player]
-   where 
+ where 
      i = fromIntegral x
      j = fromIntegral y
      player = head imagem
 
+desenhaEstado (ModoJogo1, Jogo (Jogador (x,y)) (mapaInicial),imagem,n) = Pictures $ (desenhaMapa (ModoJogo, Jogo (Jogador (x,y)) (mapaInicial),imagem,n) (-900) (515)) ++ (desenhaObsAux (ModoJogo, Jogo (Jogador (x,y)) (mapaInicial), imagem, n) (-900) (515)) ++ [Translate i j $ (!!) imagem 17]
+   where 
+     i = fromIntegral x
+     j = fromIntegral y
+     
 
 desenhaMapa :: World -> Float -> Float -> [Picture]
 desenhaMapa (ModoJogo, Jogo (Jogador (x,y)) (Mapa l []),image,n) x1 y1 = [circle 1]
@@ -138,42 +152,47 @@ desenhaObsAux (ModoJogo, Jogo (Jogador (x,y)) (Mapa l []), imagem, n) x1 y1 = [c
 desenhaObsAux (ModoJogo, Jogo (Jogador (x,y)) (Mapa l ((ter, o):t)), imagem, n) x1 y1 = desenhaObs (ModoJogo, Jogo (Jogador (x,y)) (Mapa l ((ter, o):t)), imagem, n) x1 y1 ++ desenhaObsAux (ModoJogo, Jogo (Jogador (x,y)) (Mapa l t), imagem, n) x1 (y1-90)
 
 
-  --(deslizaJogo 0 (Jogo (Jogador p) (mapaInicial)))
-
-{-moveMapa :: Key -> Jogo -> Jogo
-moveMapa 
--}
---do num <- randomR (0,9::Int) era suposto gerar um numero para a deslizaJogo noa 
 
 event :: Event -> World -> World
 event (EventKey (SpecialKey KeyEnter) Down _ _) (Opcoes Jogar, jogo, i, pont) = (ModoJogo, jogo, i, pont)
+event (EventKey (SpecialKey KeyEnter) Down _ _) (Opcoes Skins, jogo, i, pont) = (ModoSkins Skin1, jogo, i, pont)
 event (EventKey (SpecialKey KeyUp) Down _ _) (Opcoes Jogar, jogo, i, pont) = (Opcoes Sair, jogo, i, pont)
-event (EventKey (SpecialKey KeyDown) Down _ _) (Opcoes Jogar, jogo, i, pont) = (Opcoes Sair, jogo, i, pont)
-event (EventKey (SpecialKey KeyUp) Down _ _) (Opcoes Sair, jogo, i, pont) = (Opcoes Jogar, jogo,i, pont)
+event (EventKey (SpecialKey KeyUp) Down _ _) (Opcoes Sair, jogo, i, pont) = (Opcoes Skins, jogo, i, pont)
+event (EventKey (SpecialKey KeyUp) Down _ _) (Opcoes Skins, jogo, i, pont) = (Opcoes Jogar, jogo, i, pont)
+event (EventKey (SpecialKey KeyDown) Down _ _) (Opcoes Jogar, jogo, i, pont) = (Opcoes Skins, jogo, i, pont)
+event (EventKey (SpecialKey KeyDown) Down _ _) (Opcoes Skins, jogo, i, pont) = (Opcoes Sair, jogo, i, pont)
 event (EventKey (SpecialKey KeyDown) Down _ _) (Opcoes Sair, jogo,i, pont) = (Opcoes Jogar, jogo,i, pont)
 event (EventKey (SpecialKey KeyEnter) Down _ _) (Opcoes Sair, jogo, i, pont) = error "Jogo Fechou"
+
+
 --continuar a jogar depois de vencer
 event (EventKey (SpecialKey KeyEnter) Down _ _) (PerdeuJogo, jogo, i ,pont) = estadoInicial i
+--menu skins
+event (EventKey (SpecialKey KeyRight) Down _ _) (ModoSkins Skin1, jogo, i, pont) = (ModoSkins Skin2, jogo, i, pont)
+event (EventKey (SpecialKey KeyLeft) Down _ _) (ModoSkins Skin1, jogo, i, pont) = (ModoSkins Skin2, jogo, i, pont)  
+event (EventKey (SpecialKey KeyRight) Down _ _) (ModoSkins Skin2, jogo, i, pont) = (ModoSkins Skin1, jogo, i, pont)
+event (EventKey (SpecialKey KeyLeft) Down _ _) (ModoSkins Skin2, jogo, i, pont) = (ModoSkins Skin1, jogo, i, pont)
+event (EventKey (SpecialKey KeyEnter) Down _ _) (ModoSkins Skin1, jogo, i, pont) = (ModoJogo, jogo, i, pont)
+event (EventKey (SpecialKey KeyEnter) Down _ _) (ModoSkins Skin2, jogo, i, pont) = (ModoJogo1, jogo, i, pont)
 
- 
---Andar para tras com o tempo
---event _ (ModoJogo, (Jogo (Jogador (x,y)) (mapaInicial)), i, pont) = (ModoJogo, (Jogo(Jogador (x,y)) (mapaInicial)),i, pont)
-
+--modo Jogo
 event (EventKey (SpecialKey KeyUp) Down _ _) (ModoJogo, (Jogo (Jogador (x,y)) (mapaInicial)), i, pont) = if jogoTerminou (Jogo (player(Jogador (x,y)) (mapaInicial) (Move Cima)) mapaInicial) == True then (PerdeuJogo,(Jogo (Jogador (x,y)) (mapaInicial)),i,pont) else (ModoJogo, (Jogo (player(Jogador (x,y)) (mapaInicial) (Move Cima)) mapaInicial), i, pont)
 event (EventKey (SpecialKey KeyDown) Down _ _) (ModoJogo, (Jogo (Jogador (x,y)) (mapaInicial)), i, pont) = if jogoTerminou (Jogo (player(Jogador (x,y)) (mapaInicial) (Move Baixo)) mapaInicial) == True then (PerdeuJogo,(Jogo (Jogador (x,y)) (mapaInicial)),i,pont) else (ModoJogo, (Jogo (player(Jogador (x,y)) (mapaInicial) (Move Baixo)) mapaInicial), i, pont)
 event (EventKey (SpecialKey KeyRight) Down _ _) (ModoJogo, (Jogo (Jogador (x,y)) (mapaInicial)), i, pont) = if jogoTerminou (Jogo (player(Jogador (x,y)) (mapaInicial) (Move Direita)) mapaInicial) == True then (PerdeuJogo,(Jogo (Jogador (x,y)) (mapaInicial)),i,pont) else (ModoJogo, (Jogo (player(Jogador (x,y)) (mapaInicial) (Move Direita)) mapaInicial), i, pont)
 event (EventKey (SpecialKey KeyLeft) Down _ _) (ModoJogo, (Jogo (Jogador (x,y)) (mapaInicial)), i, pont) = if jogoTerminou (Jogo (player(Jogador (x,y)) (mapaInicial) (Move Esquerda)) mapaInicial) == True then (PerdeuJogo,(Jogo (Jogador (x,y)) (mapaInicial)),i,pont) else (ModoJogo, (Jogo (player(Jogador (x,y)) (mapaInicial) (Move Esquerda)) mapaInicial), i, pont)
 
+event (EventKey (SpecialKey KeyUp) Down _ _) (ModoJogo1, (Jogo (Jogador (x,y)) (mapaInicial)), i, pont) = if jogoTerminou (Jogo (player(Jogador (x,y)) (mapaInicial) (Move Cima)) mapaInicial) == True then (PerdeuJogo,(Jogo (Jogador (x,y)) (mapaInicial)),i,pont) else (ModoJogo1, (Jogo (player(Jogador (x,y)) (mapaInicial) (Move Cima)) mapaInicial), i, pont)
+event (EventKey (SpecialKey KeyDown) Down _ _) (ModoJogo1, (Jogo (Jogador (x,y)) (mapaInicial)), i, pont) = if jogoTerminou (Jogo (player(Jogador (x,y)) (mapaInicial) (Move Baixo)) mapaInicial) == True then (PerdeuJogo,(Jogo (Jogador (x,y)) (mapaInicial)),i,pont) else (ModoJogo1, (Jogo (player(Jogador (x,y)) (mapaInicial) (Move Baixo)) mapaInicial), i, pont)
+event (EventKey (SpecialKey KeyRight) Down _ _) (ModoJogo1, (Jogo (Jogador (x,y)) (mapaInicial)), i, pont) = if jogoTerminou (Jogo (player(Jogador (x,y)) (mapaInicial) (Move Direita)) mapaInicial) == True then (PerdeuJogo,(Jogo (Jogador (x,y)) (mapaInicial)),i,pont) else (ModoJogo1, (Jogo (player(Jogador (x,y)) (mapaInicial) (Move Direita)) mapaInicial), i, pont)
+event (EventKey (SpecialKey KeyLeft) Down _ _) (ModoJogo1, (Jogo (Jogador (x,y)) (mapaInicial)), i, pont) = if jogoTerminou (Jogo (player(Jogador (x,y)) (mapaInicial) (Move Esquerda)) mapaInicial) == True then (PerdeuJogo,(Jogo (Jogador (x,y)) (mapaInicial)),i,pont) else (ModoJogo1, (Jogo (player(Jogador (x,y)) (mapaInicial) (Move Esquerda)) mapaInicial), i, pont)
+
 --Nao reagir caso não aconteçam os casos em cima
 event _ x = x
-
-{-updateMapa :: ViewPort -> Float -> World -> World
-updateMapa ((-900),(-515)) (ModoJogo, (Jogo (Jogador (x,y)) (mapaInicial)), i, pont)  = (ModoJogo, animaJogo(Jogo (Jogador (x,y)) (mapaInicial)) (Move Cima), i, pont)
-funçao que faz os carros e troncos moverem em funçao do tempo, continuar-}
 
 
 pontu :: Float -> World -> World
 pontu p (ModoJogo, j, i, pont) = if jogoTerminou (animaJogo j (Parado)) == True then (PerdeuJogo,j,i,pont) else (ModoJogo, animaJogo j (Parado), i, pont+p)
+pontu p (ModoJogo1, j, i, pont) = if jogoTerminou (animaJogo j (Parado)) == True then (PerdeuJogo,j,i,pont) else (ModoJogo1, animaJogo j (Parado), i, pont+p)
 --pontu p (ModoJogo, i, j, pont) = (ModoJogo, deslizaJogo num j (Parado), i, pont)
 pontu p (PerdeuJogo, j, i, pont) = (PerdeuJogo, j, i, pont)
 pontu p (o,j,i,pont) = (o,j,i,pont+p)
@@ -201,19 +220,24 @@ corRio =  dark $ corFundo
 
 main :: IO ()
 main = do
-  character <- loadBMP "character.bmp"
-  agua      <- loadBMP "agua.bmp"
-  estrada   <- loadBMP "estrada.bmp"
-  relva     <- loadBMP "relva_1_.bmp"
-  arvore    <- loadBMP "arvore1.bmp"
-  tronco    <- loadBMP "tronco.bmp"
-  carro1    <- loadBMP "car.bmp"
-  carro3    <- loadBMP "CarroAmarelo.bmp"
-  fundo     <- loadBMP "pixel-art.bmp"
-  jogar     <- loadBMP "jogar.bmp"
-  sair      <- loadBMP "sair.bmp"
-  score     <- loadBMP "score.bmp"
-  papel     <- loadBMP "papel.bmp"
+  character   <- loadBMP "character.bmp"
+  agua        <- loadBMP "agua.bmp"
+  estrada     <- loadBMP "estrada.bmp"
+  relva       <- loadBMP "relva_1_.bmp"
+  arvore      <- loadBMP "arvore1.bmp"
+  tronco      <- loadBMP "tronco.bmp"
+  carro1      <- loadBMP "car.bmp"
+  carro3      <- loadBMP "CarroAmarelo.bmp"
+  fundo       <- loadBMP "pixel-art.bmp"
+  jogar       <- loadBMP "jogar.bmp"
+  sair        <- loadBMP "sair.bmp"
+  score       <- loadBMP "score.bmp"
+  papel       <- loadBMP "papel.bmp"
+  personagens <- loadBMP "Personagens.bmp"
+  p1          <- loadBMP "p1.bmp"
+  p2          <- loadBMP "p2.bmp"
+  pinkmonster <- loadBMP "Pink_Monster.bmp"
+  pinkmonster1<- loadBMP "Pink_Monster_Walk.bmp"
   let imagem = [scale 3 3 character,
                 scale 1 0.4 agua,
                 scale 0.3 0.143 estrada,
@@ -226,7 +250,12 @@ main = do
                 scale 1 1 jogar,
                 scale 1 1 sair,
                 scale 1 1 score,
-                scale 0.7 0.6 papel]
+                scale 1.1 0.6 papel,
+                scale 1.3 1.3 personagens,
+                scale 1 1 p1,
+                scale 1 1 p2,
+                scale 7 7 pinkmonster,
+                scale 3 3 pinkmonster1]
   play window corFundo fr (estadoInicial imagem) desenhaEstado event pontu
 
 
